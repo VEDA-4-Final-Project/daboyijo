785d6a53b9a6017172e2ed877b6595106db01b9b
mayoina <ryals29@naver.com>
2026-07-09 16:25:12 +0900

    feat: Probe 함수 수정(PARK-57)

--- alert-node/wm8960/wm8960_custum_daboyjo.c
+++ alert-node/wm8960/wm8960_custum_daboyjo.c
@@ -3,6 +3,26 @@
 #include <linux/i2c.h>
 #include <linux/mod_devicetable.h>
 
+static int veda_wm8960_write(struct i2c_client *client, uint8_t addr, uint8_t data_first, uint8_t data, char *messge)
+{
+    
+    uint8_t buf[2];
+    
+    buf[0] = addr << 1;
+    buf[0] = buf[0] | data_first;
+    buf[1] = data;
+
+    int check = i2c_master_send(client, buf, 2);
+
+    if( check == 2){
+        dev_info(&client->dev, "wm8960 %s success\n",messge);
+        return 0;
+    }else{
+        dev_info(&client->dev, "wm8960 %s fail\n",messge);
+        return check;
+    }
+}
+
 
 static int veda_wm8960_probe(struct i2c_client *client,const struct i2c_device_id *id)
 {
@@ -10,8 +30,30 @@ static int veda_wm8960_probe(struct i2c_client *client,const struct i2c_device_i
     dev_info(&client->dev, "I2C 장치 주소 0x%02x\n", client->addr);
 
     /*TOTO:
+    
 
     */
+
+    int ret;
+    // reset 
+    // 0Fh 0 0000 0000
+    ret = veda_wm8960_write(client,0x0F,0,0x00,"reset");
+    if(ret<0) return ret;
+
+    // power mgmt
+    // 19h 0 1100 0000
+    ret = veda_wm8960_write(client,0x19,0,0xC0,"power up");
+    if(ret<0) return ret;
+
+    //audio interface
+    // 07h 0 0000 0010 
+    ret = veda_wm8960_write(client,0x07,0,0x02,"audio inerface setting");
+    if(ret<0) return ret;
+
+    
+
+
+
 
     return 0;
 }
