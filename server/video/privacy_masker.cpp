b4a19ccefb9527896a4b84e67ffaea079193f076
jsh0102 <jsh190107@naver.com>
2026-07-14 17:35:14 +0900

    fix : 블러 모양 수정

--- server/video/privacy_masker.cpp
+++ server/video/privacy_masker.cpp
@@ -37,8 +37,21 @@ void PrivacyMasker::process(int channel, cv::Mat& image, const std::vector<Detec
 
             // 이미지 경계 밖 예외 방지 및 유효성 검사
             if (face_roi.width > 0 && face_roi.height > 0) {
-                cv::GaussianBlur(image(face_roi), image(face_roi), 
+                // 1. 얼굴 영역만 싹둑 잘라내서 복사본을 만들고 블러 처리하기
+                cv::Mat roi_blur = image(face_roi).clone();
+                cv::GaussianBlur(roi_blur, roi_blur, 
                                  cv::Size(blur_kernel_size_, blur_kernel_size_), 20);
+
+                // 2. 얼굴 크기만한 까만색 도화지(마스크) 만들기
+                cv::Mat mask = cv::Mat::zeros(face_roi.size(), CV_8UC1);
+
+                // 3. 까만 도화지 중심에 흰색(255)으로 꽉 찬 타원(얼굴 형태) 그리기
+                cv::Point center(face_roi.width / 2, face_roi.height / 2);
+                cv::Size axes(face_roi.width / 2, face_roi.height / 2);
+                cv::ellipse(mask, center, axes, 0, 0, 360, cv::Scalar(255), cv::FILLED);
+
+                // 4. [핵심] 흰색 타원 영역만 원본 이미지 위에 싹 덮어씌우기 (마스킹 카피)
+                roi_blur.copyTo(image(face_roi), mask);
             }
         }
     }
